554d3a206b55592e3bc1d05ebfeaff164c7435e3
bvonstvieth <b.von.st.vieth@fz-juelich.de>
2014-07-17 13:32:16 +0200

    ugly sample implementation of different auth types, type defined using a config file and corresponding import. auth only required for POST and DELETE.

---
+++ app/auth/basic.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+from functools import wraps
+from flask import abort, request, Response
+
+
+def check_auth(username, password):
+    return username == 'admin' and password == 'secret'
+
+
+def authenticate():
+    """Sends a 401 response that enables basic auth"""
+    return Response(
+        'Could not verify your access level for that URL.\n'
+        'You have to login with proper credentials', 401,
+        {'WWW-Authenticate': 'Basic realm="Login Required"'}
+    )
+
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        if not auth:
+            return authenticate()
+        if not check_auth(auth.username, auth.password):
+            abort(403)
+        return f(*args, **kwargs)
+
+    return decorated
